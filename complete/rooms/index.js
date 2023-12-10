96f5a9f085c0338462d37ea7174c4f8d3d5ec1fb
Nik Lever <47000827+NikLever@users.noreply.github.com>
2023-12-10 07:44:03 +0000

    Rooms complete

--- complete/rooms/index.js
+++ complete/rooms/index.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
 	  console.log(`A new user joined room ${room}`);
 	  socket.room = room;
 	  socket.join(room);
-  	  io.emit('rooms', getRooms('joined room'));
+    io.emit('rooms', getRooms('joined room'));
   });
 
   socket.on('chat message', (data) => {
@@ -53,27 +53,45 @@ server.listen(3000, () => {
 });
 
 function getRooms(msg){
-  const nsp = io.of('/');
-  const rooms = nsp.adapter.rooms;
+  const rooms = io.sockets.adapter.rooms;
+  /*const rooms = [];
+  const keys = Object.keys(io.sockets.sockets);
+  console.log(`getRooms: ${msg} keys:${JSON.stringify(keys)}`);
+  
+  keys.forEach( key => {
+    const socket = io.sockets[key];
+    if (socket && socket.rooms){
+      const roomKeys = Object.keys(socket.rooms);
+      roomKeys.forEach( roomKey => {
+        const room = socket.rooms[roomKey];
+        if ( rooms.indexOf( room ) == -1) rooms.push( room );
+      })
+    }
+  });*/
+
   /*Returns data in this form
   {
     'roomid1': { 'socketid1', socketid2', ...},
     ...
   }
   */
-  //console.log('getRooms rooms>>' + util.inspect(rooms));
+  console.log('getRooms rooms>>' + inspect(rooms));
 
   const list = {};
 	
-  for(let roomId in rooms){
-	  const room = rooms[roomId];
+  for(let [roomId, room] of rooms){
+	  //const room = rooms[roomId];
 	  if (room===undefined) continue;
 	  const sockets = [];
 	  let roomName = "";
 	  console.log(`getRooms room>>${inspect(room)}`);
-	  for(let socketId in room.sockets){
-		  const socket = nsp.connected[socketId];
-		  if (socket===undefined || socket.username===undefined || socket.room===undefined) continue;
+  
+	  for(let socketId of room){
+      const socket = io.sockets.sockets.get(socketId);
+      console.log(`getRooms: socket(${socketId})>>${socket}`);
+		  if (socket===undefined || socket.username===undefined || socket.room===undefined){
+         continue;
+      }
 		  console.log(`getRooms socket(${socketId})>>${socket.username}:${socket.room}`);
 		  sockets.push(socket.username);
 		  if (roomName=="") roomName = socket.room;
